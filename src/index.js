15e792040739f190039303ef47d7633f071bca26
Cristobal Heiss <barbacoabq@gmail.com>
2019-11-10 19:34:30 +0100

    basic functionality working, html output

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import binaryToDecimal from './binaryToDecimal.js'
 
+let button = document.getElementById("btn");
+let binaryValue = document.getElementById("binary-value");
 let output = document.getElementById("number-output");
-output.innerHTML = binaryToDecimal(1001);
+
+button.addEventListener("click", function(){
+  output.innerHTML = `${binaryValue.value} is: ${binaryToDecimal(binaryValue.value)}`;
+  binaryValue.value = 0;
+})
+
